5e893d44fc9ea6dee3c85789f64dbbe0d82bbfca
vinsky001 <ekibet544@gmail.com>
2023-10-31 19:03:25 +0000

    function with the babel.localeselector decorator

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""
+Get locale from request
+"""
+from flask import Flask, request, render_template
+from flask_babel import Babel
+
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config(object):
+    """configure available languages"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object('2-app.Config')
+
+
+@babel.localeselector
+def get_locale():
+    """determine best match"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """return a simple template"""
+    return render_template('2-index.html')
