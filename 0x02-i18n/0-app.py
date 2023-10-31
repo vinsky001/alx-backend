85e0e23f85931592d42ceb16f9500cd0fb5982e4
vinsky001 <ekibet544@gmail.com>
2023-10-31 18:42:08 +0000

    Basic Flask app

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Basic Flask app
+"""
+
+
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """returns a template"""
+    return render_template('0-index.html')  
